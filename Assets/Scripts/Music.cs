1d5c4fe2cece6272dee00a3ddcffd2c60f18c318
Amila Sampath <lucksy@gmail.com>
2021-05-28 10:59:42 +0200

    Play on Painting

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource flute_1;
+    public AudioSource violin_1;
+    public AudioSource organ_1;
+    public AudioSource drum_1;
+    public AudioSource trumpet_1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TriggerFlute_1()
+    {
+        if (!flute_1.isPlaying)
+        {
+            flute_1.Play();
+        } else
+        {
+            flute_1.Stop();
+        }
+    }
+
+    public void TriggerViolin()
+    {
+        if (!violin_1.isPlaying)
+        {
+            violin_1.Play();
+        }
+        else
+        {
+            violin_1.Stop();
+        }
+    }
+
+    public void TriggerOrgan()
+    {
+        if (!organ_1.isPlaying)
+        {
+            organ_1.Play();
+        }
+        else
+        {
+            organ_1.Stop();
+        }
+    }
+
+    public void TriggerDrum()
+    {
+        if (!drum_1.isPlaying)
+        {
+            drum_1.Play();
+        }
+        else
+        {
+            drum_1.Stop();
+        }
+    }
+
+    public void TriggerTrumpet()
+    {
+        if (!trumpet_1.isPlaying)
+        {
+            trumpet_1.Play();
+        }
+        else
+        {
+            trumpet_1.Stop();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Select.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select : MonoBehaviour
+{
+
+
+    public GameObject open;
+    public GameObject restless_painting;
+    public GameObject sad_painting;
+    public GameObject play_painting;
+    public GameObject calm_painting;
+
+    public bool restless = false;
+    public bool sad = false;
+    public bool play = false;
+    public bool calm = false;
+
+    public AudioSource restless_painting_audio;
+    public AudioSource sad_painting_audio;
+    public AudioSource play_painting_audio;
+    public AudioSource calm_painting_audio;
+
+    public bool update = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // calm_painting = GameObject.Find("calm_painting").GetComponent<GameObject>();
+
+        restless_painting.SetActive(false);
+        sad_painting.SetActive(false);
+        play_painting.SetActive(false);
+        calm_painting.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(update) { 
+            if (restless)
+            {
+                open.SetActive(false);
+                restless_painting.SetActive(true);
+                sad_painting.SetActive(false);
+                play_painting.SetActive(false);
+                calm_painting.SetActive(false);
+
+                restless_painting_audio.Play();
+                sad_painting_audio.Stop();
+                play_painting_audio.Stop();
+                calm_painting_audio.Stop();
+
+                update = false;
+
+            }
+            if (sad)
+            {
+                open.SetActive(false);
+                restless_painting.SetActive(false);
+                sad_painting.SetActive(true);
+                play_painting.SetActive(false);
+                calm_painting.SetActive(false);
+
+                restless_painting_audio.Stop();
+                sad_painting_audio.Play();
+                play_painting_audio.Stop();
+                calm_painting_audio.Stop();
+
+                update = false;
+
+            }
+            if (play)
+            {
+                open.SetActive(false);
+                restless_painting.SetActive(false);
+                sad_painting.SetActive(false);
+                play_painting.SetActive(true);
+                calm_painting.SetActive(false);
+
+                restless_painting_audio.Stop();
+                sad_painting_audio.Stop();
+                play_painting_audio.Play();
+                calm_painting_audio.Stop();
+
+                update = false;
+            }
+            if (calm)
+            {
+                open.SetActive(false);
+                restless_painting.SetActive(false);
+                sad_painting.SetActive(false);
+                play_painting.SetActive(false);
+                calm_painting.SetActive(true);
+
+                restless_painting_audio.Stop();
+                sad_painting_audio.Stop();
+                play_painting_audio.Stop();
+                calm_painting_audio.Play();
+                update = false;
+            }
+        }
+    }
+
+    public void TriggerRestless()
+    {
+        restless = true;
+        sad = false;
+        play = false;
+        calm = false;
+        update = true;
+    }
+
+    public void TriggerSad()
+    {
+        restless = false;
+        sad = true;
+        play = false;
+        calm = false;
+        update = true;
+    }
+
+    public void TriggerPlay()
+    {
+        restless = false;
+        sad = false;
+        play = true;
+        calm = false;
+        update = true;
+    }
+
+    public void TriggerCalm()
+    {
+        restless = false;
+        sad = false;
+        play = false;
+        calm = true;
+        update = true;
+    }
+
+}
